558bdf7199e038a3dfc2db7442427030271d8d83
DwanW <kunvang@yahoo.com>
2021-05-03 23:51:30 -0600

    starting front end

---
+++ web/components/Layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface LayoutProps {
+
+}
+
+const Layout: React.FC<LayoutProps> = ({}) => {
+        return (
+            <div>
+                Layout
+            </div>
+        );
+}
+
+export default Layout
