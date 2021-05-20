8e4cb465f98f82693d7ad883fec9a20e5a91df68
DwanW <kunvang@yahoo.com>
2021-05-19 22:35:24 -0600

    added carousel

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,11 +1,25 @@
-// import Head from "next/head";
-// import Image from "next/image";
-
 import { useState } from "react";
 import Card from "../components/Containers/Card";
 import StoryNavbar from "../components/Navbars/StoryNavbar";
-import StoryCoverUpload from "../components/Uploads/StoryCoverUpload";
+// import StoryCoverUpload from "../components/Uploads/StoryCoverUpload";
 import { useStoriesNewQuery } from "../generated/graphql";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 export default function Home() {
   const [storyNewCursor, setStoryNewCursor] = useState(null);
@@ -17,17 +31,25 @@ export default function Home() {
     },
   });
 
-  // console.log({data});
+  if(!data){
+    return(
+      <div>loading...</div>
+    )
+  }
   return (
     <>
       <StoryNavbar />
-      <div className="mt-16 container mx-auto px-8 pb-2 font-medium text-2xl border-b-2 border-gray-100">New Stories</div>
-      <div className="container mx-auto px-4 min-h-full flex flex-col sm:flex-row flex-wrap">
-        {data?.getNewStories.stories.map((story, idx) => (
-          <div className="my-4 mx-4">
-            <Card key={idx} title={story.title} coverUrl={story.cover_url} />
-          </div>
-        ))}
+      <div className="mt-16 container mx-auto px-8 pb-2 font-medium text-2xl border-b-2 border-gray-100">
+        New Stories
+      </div>
+      <div className="">
+        <Carousel responsive={responsive} ssr={true}>
+          {data?.getNewStories.stories.map((story, idx) => (
+            <div className="" key={idx}>
+              <Card title={story.title} coverUrl={story.cover_url} />
+            </div>
+          ))}
+        </Carousel>
       </div>
       {/* <StoryCoverUpload storyId={3} /> */}
     </>
