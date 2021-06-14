c64dcc151a7c4af2446311014c03a2d34ff14573
DwanW <kunvang@yahoo.com>
2021-06-13 19:04:23 -0600

    md

--- web/components/Forms/MDEditor.tsx
+++ web/components/Forms/MDEditor.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
-import gfm from 'remark-gfm'
+import gfm from "remark-gfm";
 
 interface MDEditorProps {}
 
@@ -8,18 +8,20 @@ const MDEditor: React.FC<MDEditorProps> = ({}) => {
   const [mdText, setMdText] = useState("");
 
   return (
-    <div>
-      <h4>My MD Editor</h4>
-      <div className="container mx-auto border border-red-200 min-h-screen">
-        <div className="mt-2 mx-2 border border-purple-200">
-          <textarea
-            className="form-textarea w-full min-h"
-            value={mdText}
-            onChange={(e) => setMdText(e.target.value)}
+    <div className="container mx-auto border border-red-200 min-h-screen">
+      <h4 className="text-4xl text-center">My MD Editor</h4>
+      <div className="mt-2 mx-2 border border-purple-200">
+        <textarea
+          className="w-full min-h-[200px]"
+          value={mdText}
+          onChange={(e) => setMdText(e.target.value)}
+        />
+        <div className="prose min-h-[200px]">
+          <ReactMarkdown
+            children={mdText}
+            remarkPlugins={[[gfm, { singleTilde: false }]]}
+            className="px-3 py-2 w-full"
           />
-          <div className="prose">
-            <ReactMarkdown children={mdText} remarkPlugins={[[gfm, {singleTilde: false}]]} />
-          </div>
         </div>
       </div>
     </div>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
+  theme: {},
+  variants: {},
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
